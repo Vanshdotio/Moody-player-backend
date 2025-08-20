cef602c60efcfb90af73beb704cedac82fbce867
Vansh Sharma <sharmavandita2004@gmail.com>
2025-08-20 22:50:04 +0530

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const app = require('../Backend/src/app');
+const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const port = process.env.PORT || 3000;
 
 connectDB();
 
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Server is running on port 3000');
